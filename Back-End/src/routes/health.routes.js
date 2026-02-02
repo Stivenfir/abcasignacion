6b54799940b743cf048155a66641cf5e68664ff0
jdgrabc <114615875+jdgrabc@users.noreply.github.com>
2026-02-02 15:11:29 -0500

    fecht para comunicarse con la BD
    health.routes.js
    
    Se agrego un fecht y una funcion asincrona para hacer peticionaes a la BD

--- Back-End/src/routes/health.routes.js
+++ Back-End/src/routes/health.routes.js
@@ -5,29 +5,53 @@ console.log("DB exports:", Object.keys(db)); // debe mostrar: ['getConnection','
 
 const router = Router();
 
+async function GetData(P){
+  return new Promise(resolve=>{
+    var OptPet={
+      method: 'POST',
+      headers:{
+        'Content-Type': 'application/x-www-form-urlencoded',
+        TK:process.env.API_TOKEN 
+      },
+      body: P.replace('/\n/g',"\\n")
+    }
+    fetch(process.env.EXTERNAL_API_URL,OptPet).then(response=>response.text()).then(Rta=>{
+      resolve(Rta)
+    }).catch(error=>{console.error("Error",error);});
+  });
+}
+
 router.get("/", (req, res) => {
   res.json({ status: "ok", service: "desk-booking-api" });
 });
 
 // Ruta de prueba de DB
 router.get("/db-test", async (req, res) => {
-  try {
-    const pool = await db.getConnection();
-    const result = await pool.request().query("SELECT @@VERSION as version");
-
-    res.json({
-      status: "ok",
-      message: "Conexión exitosa a SQL Server",
-      version: result.recordset[0].version
-    });
-  } catch (error) {
-    console.error("DB TEST ERROR:", error);
-    res.status(500).json({
-      status: "error",
-      message: "Error conectando a la base de datos",
-      error: error.message
-    });
+  var Rta = await GetData("GetPisos=''");
+  try{
+    var S=Rta.trim();
+    var D=JSON.parse(S.trim())["data"];
+    return(res.json(D));
   }
+  catch(error){
+    console.error('Error:', error);
+    return res.status(401).json({ message: error});  
+  }
+  
+});
+
+router.get("/pisos", async (req, res) => {
+  var Rta = await GetData("GetPisos=''");
+  try{
+    var S=Rta.trim();
+    var D=JSON.parse(S.trim())["data"];
+    return(res.json(D));
+  }
+  catch(error){
+    console.error('Error:', error);
+    return res.status(401).json({ message: error});
+  }
+  
 });
 
 export default router;
